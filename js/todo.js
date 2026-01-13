ca8259b5ef8d5fca97e608026815712000680374
jasmeettt <jasmeetsalhotrawork@gmail.com>
2026-01-14 01:22:33 +0530

    Refactor todo application styles and functionality
    - Updated CSS styles for completed tasks to improve visibility.
    - Modified HTML structure to remove unnecessary 'completed' class from task list items.
    - Enhanced JavaScript functionality to handle task deletion and checkbox state changes.
    - Added event listener for 'Escape' key to close the add task input.
    - Cleaned up code for better readability and maintainability.

--- js/todo.js
+++ js/todo.js
@@ -6,8 +6,8 @@ export const todo = () => {
   addButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
       //   console.log(btn);
-      //   console.log(day);
       const day = btn.closest(".todo-day");
+      console.log(day);
 
       if (activeAddDay && activeAddDay !== day) {
         closeAddMode(activeAddDay);
@@ -35,12 +35,18 @@ export const todo = () => {
         closeAddMode(day);
       }
     });
+
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        closeAddMode(day);
+      }
+    });
   }
 
   function closeAddMode(day) {
     const btn = day.querySelector(".add-task-btn");
     const inputBox = day.querySelector(".add-task-input");
-    const input = day.querySelector("input");
+    const input = day.querySelector(".add-task-input input");
 
     btn.classList.remove("hidden");
     inputBox.classList.add("hidden");
@@ -51,6 +57,7 @@ export const todo = () => {
 
   function addTask(day, text) {
     const taskList = day.querySelector(".task-list");
+    const input = day.querySelector(".add-task-input input");
 
     const li = document.createElement("li");
     li.className = "task";
@@ -60,8 +67,33 @@ export const todo = () => {
                   <span class="task-text">
                     ${text}
                   </span>
-                  </div>`;
+                  </div>
+                   <i class="ri-delete-bin-5-fill delete"></i>
+                  `;
 
     taskList.appendChild(li);
+
+    input.value = "";
   }
+
+  //looping through tasklist to add practicality to delete and checkbox
+  const taskList = document.querySelectorAll(".task-list");
+  // console.log(taskList)
+  taskList.forEach((list) => {
+    // console.log(list)
+    list.addEventListener("click", (e) => {
+      const task = e.target.closest(".task");
+      if (!task) return;
+
+      if (e.target.classList.contains("delete")) {
+        task.remove();
+        return;
+      }
+      if(e.target.matches("input[type = 'checkbox'")){
+        task.classList.toggle("completed", e.target.checked)
+      }
+      // if(e.)
+      // console.log(e.target.closest(".task"));
+    });
+  });
 };
